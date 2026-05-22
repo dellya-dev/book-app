04f8c44f11a6b9a4efe1f33c6c21a726c036b19c
Dellya <dellyadellya061@gmail.com>
2026-05-22 10:53:01 +0700

    Create state and fetch API

--- book-app/src/App.jsx
+++ book-app/src/App.jsx
@@ -2,9 +2,35 @@ import './App.css'
 import SearchBar from './components/SearchBar'
 import RecommendedSection from './components/RecommendedSection'
 import BookList from './components/BookList'
+import { useState } from 'react'
 
 function App() {
- const books = [{
+  const [query, setQuery] = useState("")
+  const [books, setBooks] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  async function fetchBooks() {
+    setLoading(true)
+    const response = await fetch ("fetch(`https://openlibrary.org/search.json?q=${query}`)")
+    const data = await response.json()
+    setBooks(data.docs.map((book) => {
+      return { 
+        title: book.title,
+        author: book.author_name?.[0],
+        year: book.first_publish_year,
+        language: book.language?.[0]
+      }
+    }))
+    setLoading(false)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    fetchBooks()
+  }
+
+ const DummyBooks = [{
   id: 1,
   title: "Matahari",
   author: "Ndoro Putri",
@@ -19,16 +45,16 @@ function App() {
  }]
  console.log(books)
 
- const recommendedBooks = books.filter((book) => {
+ const recommendedBooks = DummyBooks.filter((book) => {
     return book.genre === "Fantasy"
  });
 
   return (
     <>
     <h1>LIBRARY</h1>
-    <SearchBar />
+    <SearchBar onSearch = {handleSubmit}/>
     <RecommendedSection recommendedBooks={recommendedBooks}/>
-    <BookList books={books}/>
+    <BookList books={DummyBooks}/>
     </>
   )
 }
